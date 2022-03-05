bd9c69e210711004cefd193e3b3743ac9ab12cf4
Иван <dert.0.1@yandex.ru>
2022-03-05 22:35:51 +0700

    add group model and meta classes for group and user models

--- blog_sfu_proj/settings.py
+++ blog_sfu_proj/settings.py
@@ -45,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'pages',
-    'posts',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -2,6 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Group(models.Model):
+    title = models.CharField(verbose_name='название')
+    slug = models.SlugField(unique=True, verbose_name='слаг')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
 class User(AbstractUser):
-    image = models.CharField(max_length=200)
+    image = models.CharField(
+        max_length=200,
+        blank=True,
+        verbose_name='аватарка'
+    )
+    group = models.ForeignKey(
+        Group,
+        related_name='users',
+        verbose_name='группа'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
