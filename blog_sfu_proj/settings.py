314d33af461702aa0fde4caf7ad364da6ae7e2fb
Ivan <vanya909x75@gmail.com>
2022-03-05 21:19:46 +0700

    Add pages app
    Add templated directory and base template.

--- blog_sfu_proj/settings.py
+++ blog_sfu_proj/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.pages.PagesConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'blog_sfu_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
