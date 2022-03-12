873acfd14a7b8708b3a27708d3b47a74f00cc0c6
Ivan <vanya909x75@gmail.com>
2022-03-12 19:13:05 +0700

    Update registration form
    Auto login user after registration. Add STATIC_ROOT.

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
 from .forms import CustomUserCreationForm
 from .models import StudyGroup
@@ -12,6 +13,7 @@ def signup_view(request):
             try:
                 user.group = StudyGroup.objects.filter(title=form.cleaned_data['group'].upper())[0]
                 user.save()
+                login(request, user)
                 return redirect('index')
             except IndexError:
                 form.add_error('group', 'Такой группы не найдено')
