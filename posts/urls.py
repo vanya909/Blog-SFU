c081de503a02ea5089a0d10e739fab931f58b89d
Ivan <vanya909x75@gmail.com>
2022-03-25 22:36:24 +0700

    Add create page

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import study_group_posts_view, post_detail_view
+from .views import study_group_posts_view, post_detail_view, create_post
 
 urlpatterns = [
     path('<int:pk>/', post_detail_view, name='post_detail'),
     path('group/', study_group_posts_view, name='study_group_posts'),
+    path('create/', create_post, name='post_create'),
 ]
