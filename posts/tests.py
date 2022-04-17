b98d268b3acfdff0565c85f00b2e10d8b87a625d
vanya909 <vanya909x75@gmail.com>
2022-04-17 14:02:42 +0700

    Add tests for study group posts view

--- posts/tests.py
+++ posts/tests.py
@@ -6,24 +6,20 @@ from users.models import StudyGroup
 from .models import Post
 
 
-class PostDetailTest(TestCase):
+def get_test_user(username, password, group_title):
+    """Function which returns a new user for tests"""
+    group = StudyGroup.objects.get_or_create(title=group_title, slug=group_title.lower())[0]
+    return get_user_model().objects.create_user(username=username, password=password, group=group)
+
+
+class PostDetailViewTest(TestCase):
     """Test post detail page"""
 
     def setUp(self):
         """Create two users. First will be logged in and second will not"""
-        self.first_study_group = StudyGroup.objects.create(title='ABC', slug='abc')
-        self.second_study_group = StudyGroup.objects.create(title='BCA', slug='bca')
 
-        self.first_user = get_user_model().objects.create_user(
-            username='firstuser',
-            password='12345678',
-            group=self.first_study_group
-        )
-        self.second_user = get_user_model().objects.create_user(
-            username='seconduser',
-            password='12345678',
-            group=self.second_study_group
-        )
+        self.first_user = get_test_user('firstuser', '12345678', 'ABC')
+        self.second_user = get_test_user('seconduser', '12345678', 'BCA')
 
         self.client.login(username='firstuser', password='12345678')
 
@@ -74,3 +70,54 @@ class PostDetailTest(TestCase):
 
         response = self.client.get(reverse_lazy('post_detail', kwargs={'pk': another_group_post.pk}))
         self.assertNotEqual(response.status_code, 200)
+
+
+class StudyGroupPostsViewTest(TestCase):
+    """Test study group posts page"""
+
+    def setUp(self):
+        """Create test user"""
+        self.test_user = get_test_user(username='testuser', password='12345678', group_title='ABC')
+
+    def test_study_group_posts_status_code(self):
+        """Test study group posts status code is 200"""
+        post = Post.objects.create(
+            author=self.test_user,
+            only_for_group=True,
+            title='SimpleTitle',
+            description='SimpleDescription',
+        )
+        self.client.login(username='testuser', password='12345678')
+
+        response = self.client.get('/posts/group/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_study_group_posts_status_code_reverse_lazy(self):
+        """Test post detail page status code using reverse_lazy() is 200"""
+        post = Post.objects.create(
+            author=self.test_user,
+            only_for_group=False,
+            title='SimpleTitle',
+            description='SimpleDescription',
+        )
+        self.client.login(username='testuser', password='12345678')
+
+        response = self.client.get(reverse_lazy('study_group_posts'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_study_group_posts_status_code_with_and_without_login(self):
+        """Test study group posts status code is 200 when logged in and isn't 200 when not"""
+        post = Post.objects.create(
+            author=self.test_user,
+            only_for_group=True,
+            title='SimpleTitle',
+            description='SimpleDescription',
+        )
+
+        response = self.client.get(reverse_lazy('study_group_posts'))
+        self.assertNotEqual(response.status_code, 200)
+
+        self.client.login(username='testuser', password='12345678')
+
+        response = self.client.get(reverse_lazy('study_group_posts'))
+        self.assertEqual(response.status_code, 200)
