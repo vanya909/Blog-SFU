8f2bcaf20c84ef7d257fc44397826e7aaa9bc7cc
Иван <dert.0.1@yandex.ru>
2022-06-14 23:04:36 +0700

    Add script for parse all groups sfu and chromedriver

---
+++ posts/utils.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+path = 'http://newtimetable.sfu-kras.ru/main'
+
+driver = webdriver.Chrome('../chrome_driver_for_parse/chromedriver.exe')
+driver.get(path)
+
+institutions = driver.find_elements(By.CLASS_NAME, 'instItemHead')
+
+groups_of_courses = []
+
+for institut_count in range(0, len(institutions)):
+    js_script = f'document.getElementsByClassName("instItemContainer")[{institut_count}].click();'
+
+    time.sleep(1)
+
+    driver.execute_script(js_script)
+
+    time.sleep(1)
+
+    courses = driver.find_elements(By.CLASS_NAME, 'instItemCoursesItem')
+    for course_count in range(0, len(courses)):
+        js_script = f'document.getElementsByClassName("instItemCoursesItem")[{course_count}].click();'
+
+        time.sleep(1)
+
+        driver.execute_script(js_script)
+
+        groups = driver.find_elements(By.CLASS_NAME, 'instItemGroupItem')
+        groups_titles = [group.text for group in groups]
+        groups_of_courses.append(groups_titles)
